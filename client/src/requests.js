d816825a25c6e947895991b4ef0cb558b9e5a640
zema <zlobzn@gmail.com>
2021-08-27 22:56:47 +0300

    added cors support django app; small fixes on the client side

--- client/src/requests.js
+++ client/src/requests.js
@@ -1,7 +1,12 @@
 import "regenerator-runtime/runtime";
+
+const baseUrl = "http://localhost:8000/api/v1";
+
 async function itemModification(id) {}
+
 async function itemRemove(id) {
-  let response = await fetch("http://localhost:3000/todos" + id, {
+  let response = await fetch(`${baseUrl}/todo/` + id, {
+    credentials: "omit",
     method: "DELETE",
     headers: {
       "Content-Type": "application/json; charset=UTF-8",
@@ -12,9 +17,11 @@ async function itemRemove(id) {
 
   return result;
 }
+
 async function sendNewItem(dataObj) {
   dataObj = JSON.stringify(dataObj);
-  let response = await fetch("http://localhost:3000/todos", {
+  let response = await fetch(`${baseUrl}/todo/`, {
+    credentials: "omit",
     method: "POST",
     headers: {
       Accept: "application/json",
@@ -27,12 +34,15 @@ async function sendNewItem(dataObj) {
 
   return result;
 }
+
 async function getItems() {
-  let response = await fetch("http://localhost:3000/todos", {
+  let response = await fetch(`${baseUrl}/todo/`, {
+    credentials: "omit",
     method: "GET",
   });
 
   let result = await response.json();
   return result;
 }
+
 export { sendNewItem, getItems, itemRemove };
