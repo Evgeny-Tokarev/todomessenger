0563010dc9ec1de87eced5d2db3e56cc4cf7e7ca
Evgeniy <ghenya.t@gmail.com>
2021-08-29 17:02:13 +0300

    yet another conflict

--- client/src/buttons.js
+++ client/src/buttons.js
@@ -1,5 +1,5 @@
 const buttons = document.createElement("div");
-buttons.classList.add("select-popup");
+buttons.classList.add("buttons");
 buttons.innerHTML = `
 <button class="circle-button change-button" type="button">Change</button>
 <button class="circle-button delete-button" type="button">Delete</button>
--- client/src/requests.js
+++ client/src/requests.js
@@ -1,19 +1,8 @@
 import "regenerator-runtime/runtime";
-<<<<<<< HEAD
 async function changeItem(id) {}
 async function removeItem(id) {
   console.log("Removing...");
   let response = await fetch("http://localhost:3000/todos/" + id, {
-=======
-
-const baseUrl = "http://localhost:8000/api/v1";
-
-async function itemModification(id) {}
-
-async function itemRemove(id) {
-  let response = await fetch(`${baseUrl}/todo/` + id, {
-    credentials: "omit",
->>>>>>> f06a34646e9dbed455328ab992df658c4818ab6f
     method: "DELETE",
     headers: {
       "Content-Type": "application/json; charset=UTF-8",
@@ -24,15 +13,9 @@ async function itemRemove(id) {
 
   return result;
 }
-
 async function sendNewItem(dataObj) {
   dataObj = JSON.stringify(dataObj);
-<<<<<<< HEAD
   let response = await fetch("http://localhost:8000/api/v1/todo", {
-=======
-  let response = await fetch(`${baseUrl}/todo/`, {
-    credentials: "omit",
->>>>>>> f06a34646e9dbed455328ab992df658c4818ab6f
     method: "POST",
     headers: {
       Accept: "application/json",
@@ -45,24 +28,14 @@ async function sendNewItem(dataObj) {
   console.log(result);
   return result;
 }
-
 async function getItems() {
-<<<<<<< HEAD
   let response = await fetch("http://localhost:8000/api/v1/todo", {
-=======
-  let response = await fetch(`${baseUrl}/todo/`, {
-    credentials: "omit",
->>>>>>> f06a34646e9dbed455328ab992df658c4818ab6f
     method: "GET",
     mode: "cors",
+    'Access-Control-Allow-Origin':'http://localhost:4200'
   });
 
   let result = await response.json();
   return result;
 }
-<<<<<<< HEAD
 export { sendNewItem, getItems, removeItem, changeItem };
-=======
-
-export { sendNewItem, getItems, itemRemove };
->>>>>>> f06a34646e9dbed455328ab992df658c4818ab6f
